4b6db5accbc72e2bd66a153e7833434f92b2a815
IvanSolovyevTueaccount <i.solovyev@student.tue.nl>
2026-02-02 09:58:55 +0100

    added calculation for leaf spring

--- Support/leafspring.m
+++ Support/leafspring.m
@@ -9,7 +9,7 @@ L_m = L_mm/1000;
 W_m = W_mm/1000;
 T_m = T_mm/1000;
 
-E = 1e9;
+E = 2e9;
 
 K = (E*W_m*T_m^3)/(L_m^3);
 
@@ -24,10 +24,11 @@ f_Hz = omega / (2*pi);
 disp(f_Hz)
 
 %%
-n= 3;
-w = 1;
-a = 52/3; %=h
-b = 20; 
+m= 0.5;
+n= 5;
+w = 2;
+a = 52/5; %=h
+b = 10; 
 a_m = a/1000;
 b_m = b/1000;
 w_m = w/1000;
@@ -35,3 +36,12 @@ w_m = w/1000;
 I = (a_m*w_m^3)/12;
 
 K_s = (48*E*I)/((a_m^2*n)*((a_m+b_m*n^2 - 3*b_m*n +2*b_m)));
+
+omega1 = sqrt((2*K_s)/m);
+
+% Mode numbers
+  
+
+f_Hz = omega1/ (2*pi);
+
+disp(f_Hz)
