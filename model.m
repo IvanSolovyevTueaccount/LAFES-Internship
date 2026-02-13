226094ae4fc16265f1077b8a6b0b41806e3d63c8
MichielsSFH <s.f.h.michiels@student.tue.nl>
2026-02-13 16:20:09 +0100

    Attempt at stabilizing model
    Co-Authored-By: ronghui0830 <113143290+ronghui0830@users.noreply.github.com>

--- model.m
+++ model.m
@@ -1,62 +1,50 @@
-
 clear; clc;
 
-m1 = 0.5;          
-m2 = 0.5;          
-k1 = 5e7;        
-c1 = 0;          
-k2 = 1000;         
-c2 = 0;         
-J0  = 1.53e-4;      
-r  = 0.04;         
+m1 = 0.4;          
+m2 = 0.75; 
+J0 = 4e-4;
+r  = 0.04;
 
+k_b = 6e5; 
+c_b = 6.5;     
+k_l = 7e3;
+c_l = 10;
+c_f = 1;
+k_g = 1e-10;
 
-M = [ m1      0        0;
-      0       m2       0;
-      0       0   J0/r^2 ];
+f_s = 1000;
 
-C = [ c1+c2   -c2    -c1;
-     -c2      c2      0;
-     -c1      0       c1 ];
+M = diag([ J0/r^2, m1, m2 ]);
 
-K = [ k1+k2   -k2    -k1;
-     -k2      k2      0;
-     -k1      0       k1 ];
+K = [ k_b  -k_b          0                  
+     -k_b   k_b + k_l   -k_l
+      0    -k_l          k_l + k_g ];
 
+C = [ c_b  -c_b          0                  
+     -c_b   c_b + c_l   -c_l
+      0    -c_l          c_l + c_f ];
 
-B = [ 0;
-      0;
-      1/r ];
+B = [ 1/r; 0; 0 ];
 
-%% 
-Minv = M \ eye(3);              
+dofs = size(M,1);
 
-A = [ zeros(3)  eye(3);
-     -Minv*K   -Minv*C ];
+%% Build state-space system
+% State vector:
+% x = [q1 q2 q3 q1dot q2dot q3dot]'
 
-Bss = [ zeros(3,1);
-        Minv*B ];
+A = [ zeros(dofs)      eye(dofs);
+     -M\K   -M\C ];
 
+Bss = [ zeros(dofs,1);
+         M\B ];
 
-Css = [1 0 0  0 0 0;
-       0 1 0  0 0 0];
+% Output all displacements
+C_out = [1 0 0 0 0 0
+         0 0 1 0 0 0];
 
-Dss = [0; 0];
+D = zeros(2,1);
 
-sys_ss = ss(A,Bss,Css,Dss);    
-%%
-G = tf(sys_ss);         
+sys = ss(A, Bss, C_out, D);
 
-G_d=c2d(G, 1/250, 'tustin')
-
-G1 = G(1);               
-G2 = G(2);               
-
-disp('G1(s) = X1(s) / tau(s):');
-G1
-
-disp('G2(s) = X2(s) / tau(s):');
-G2
-
-% figure()
-% bode(G1, G2); legend('x1/tau','x2/tau');
+G1 = c2d(tf(sys(1)), 1/f_s, 'tustin')
+G2 = c2d(tf(sys(2)), 1/f_s, 'tustin')
